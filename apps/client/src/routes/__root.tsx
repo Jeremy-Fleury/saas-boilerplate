dfd03764e9c440de3cb0f9d6810554e203cf72ee
jfleury <jeremy.fleury@blank.app>
2026-01-09 18:16:14 +0100

    feat(ui): init shadcn

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -2,6 +2,8 @@ import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import type { JSX } from "react";
 
+import { Button } from "@/shadcn/components/ui/button";
+
 export const Route = createRootRoute({
 	component: RootComponent,
 });
@@ -10,15 +12,9 @@ function RootComponent(): JSX.Element {
 	return (
 		<>
 			<div className="p-2 flex gap-2 text-lg">
-				<Link
-					activeOptions={{ exact: true }}
-					activeProps={{
-						className: "font-bold",
-					}}
-					to="/"
-				>
-					Home
-				</Link>
+				<Button asChild>
+					<Link to="/">Home</Link>
+				</Button>
 			</div>
 			<hr />
 			<Outlet />
